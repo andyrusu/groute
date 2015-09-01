2824e82183b83ea1b73923e374ac87ea2ed5ccd5
Andrei (Andy) Rusu <rusu.andy1@gmail.com>
2015-09-01 23:46:41 +0300

    Changes to the api'

--- docs/examples.php
+++ docs/examples.php
@@ -2,44 +2,47 @@
 $router = new Router([
 	'group'	   => '', //Set by default
 	'strategy' => Router::STRATEGY_NONE, // default value, does not make assumptions about children routes
-	[
-		'routes'  => ['index'], // results in route: /index
-		'handler' => 'indexHandler'
-	],
-	[
-		'group'    => 'posts',
-		'strategy' => Router::STRATEGY_CRUD
-		'handler'  => [
-			'class'			 => 'Controllers\Posts',
-			'methodPrefix'   => '', // is default, can be set to a string e.g.: action
-			'methodSufix'    => '', // is default, can be set to a string e.g.: action
-			'methodNameGlue' => '', // is default (no transformation applied to names), can be set to a string e.g.: camelCase
-		], // or can be shorter as: 'handler' => 'Controllers\Posts'
-		'routes' => [
-			[ //Example of overriding a route, by default route is generated automatically
-				'routes'  => ['', '(page: d+)' => ['page' => 1]],
-				'handler' => 'index',
-			],
-			//Auto-generates:
-			[
-				'routes'  => ['create'],
-				'handler' => 'create',
-			],
-			[
-				'group'  => '(id: \d+)'
-				'routes' => [
-					[
-						'routes'  => 'view',
-						'handler' => 'view',
-					],
-					[
-						'routes'  => 'update',
-						'handler' => 'update',
-					],
-					[
-						'routes'  => 'delete',
-						'handler' => 'delete',
-					],
+	'routes'   => [
+		[
+			'routes'  => ['', 'index'], // results in route: /index
+			'handler' => 'indexHandler',
+			'method'  => Router::METHOD_GET, // default value
+		],
+		[
+			'group'    => 'posts',
+			'strategy' => Router::STRATEGY_CRUD, //STRATEGY_REST
+			'handler'  => [
+				'class'			 => 'Controllers\Posts',
+				'methodPrefix'   => '', // is default, can be set to a string e.g.: action
+				'methodSufix'    => '', // is default, can be set to a string e.g.: action
+				'methodNameGlue' => '', // is default (no transformation applied to names), can be set to a string e.g.: camelCase
+			], // or can be shorter as: 'handler' => 'Controllers\Posts'
+			'routes' => [
+				//Auto-generates:
+				[
+					'routes'  => ['', 'index'], //Means: /posts/ and /posts/index
+					'handler' => 'index',
+				],
+				[
+					'routes'  => ['create'], //Means: /posts/create
+					'handler' => 'create',
+				],
+				[
+					'group'  => '(id: \d+)'
+					'routes' => [
+						[
+							'routes'  => ['', 'view'], //Means: /posts/1/view
+							'handler' => 'view',
+						],
+						[
+							'routes'  => ['update'],
+							'handler' => 'update',
+						],
+						[
+							'routes'  => ['delete'],
+							'handler' => 'delete',
+						],
+					]
 				]
 			]
 		]
